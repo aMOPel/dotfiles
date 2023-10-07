80b8b3818d14432eca385be0252b21cfb21f8717
aMOPel <momoPFL@gmail.com>
2023-10-07 19:27:48 +0200

    improved nvim-treesitter-context config

--- home/dot_config/nvim/lua/plugins/core/treesitter.lua
+++ home/dot_config/nvim/lua/plugins/core/treesitter.lua
@@ -83,6 +83,33 @@ table.insert(plugins, {
   end,
 })
 
+table.insert(plugins, {
+  name = 'nvim-treesitter-context',
+  setup = function()
+    vim.keymap.set("n", "[c", function()
+      require("treesitter-context").go_to_context()
+    end, { silent = true })
+  end,
+  config = function()
+    require'treesitter-context'.setup{
+      enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+      max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+      min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+      line_numbers = true,
+      multiline_threshold = 20, -- Maximum number of lines to show for a single context
+      trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+      mode = 'cursor',  -- Line used to calculate context. Choices: 'cursor', 'topline'
+      -- Separator between context and content. Should be a single character string, like '-'.
+      -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+      separator = nil,
+      zindex = 10000, -- The Z-index of the context window
+      on_attach = function ()
+        vim.cmd[[hi TreesitterContextBottom gui=underline guisp=Grey]]
+      end
+    }
+  end,
+})
+
 local p = require 'utils'.p
 
 local M = function(use)
@@ -91,8 +118,8 @@ local M = function(use)
     requires = {
       { p 'https://github.com/JoosepAlviste/nvim-ts-context-commentstring', after = 'nvim-treesitter', },
       { p 'https://github.com/RRethy/nvim-treesitter-textsubjects', after = 'nvim-treesitter', },
-      { p 'https://github.com/romgrk/nvim-treesitter-context', after = 'nvim-treesitter', },
       -- { p'https://github.com/jasonmchan/ts-textobjects', after = 'nvim-treesitter', },
+      { p 'https://github.com/nvim-treesitter/nvim-treesitter-context', after = 'nvim-treesitter', },
     },
     run = ':TSUpdate',
   }
